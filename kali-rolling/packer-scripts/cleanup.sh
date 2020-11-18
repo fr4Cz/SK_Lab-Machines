b3e75cb56f26552f5899f714b6367e548302868e
Stian-Kristoffersen <stian.kristoffersen@me.com>
2020-11-18 23:08:37 +0100

    Bugfixes

--- kali-rolling/packer-scripts/cleanup.sh
+++ kali-rolling/packer-scripts/cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
 # Uninstall Ansible and remove PPA.
-apt -y remove --purge ansible
+apt-get -y remove --purge ansible
 apt-add-repository --remove ppa:ansible/ansible
 
 # Apt cleanup.
-apt autoremove
-apt update
+apt-get autoremove
+apt-get update
 
 # Disable root account over SSH
 sed -i '/PermitRootLogin yes/d' /etc/ssh/sshd_config
