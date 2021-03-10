383ea12ddf265944af7c1e069655bc7bae65cd6d
Stian-Kristoffersen <stian.kristoffersen@me.com>
2021-03-10 18:09:55 +0100

    Exchange-lab 2016

---
+++ Labs/Exchange-Lab-2016/scripts/join-domain.ps1
@@ -0,0 +1,29 @@
+# Purpose: Joins a Windows host to the contoso.com domain which was created with "create-domain.ps1".
+# Source: https://github.com/StefanScherer/adfs2
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Joining the domain..."
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) First, set DNS to DC to join the domain..."
+$newDNSServers = "192.168.38.102"
+$adapters = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object {$_.IPAddress -match "192.168.38."}
+$adapters | ForEach-Object {$_.SetDNSServerSearchOrder($newDNSServers)}
+
+Write-Host "$('[{0:HH:mm}]' -f (Get-Date)) Now join the domain..."
+$hostname = $(hostname)
+$user = "contoso.com\vagrant"
+$pass = ConvertTo-SecureString "vagrant" -AsPlainText -Force
+$DomainCred = New-Object System.Management.Automation.PSCredential $user, $pass
+
+# Place the computer in the correct OU based on hostname
+If ($hostname -eq "wef") {
+  Add-Computer -DomainName "contoso.com" -credential $DomainCred -OUPath "ou=Servers,dc=contoso,dc=com" -PassThru
+} ElseIf ($hostname -eq "win10") {
+  Write-Host "Adding Win10 to the domain. Sometimes this step times out. If that happens, just run 'vagrant reload win10 --provision'" #debug
+  Add-Computer -DomainName "contoso.com" -credential $DomainCred -OUPath "ou=Workstations,dc=contoso,dc=com"
+} Else {
+  Add-Computer -DomainName "contoso.com" -credential $DomainCred -PassThru
+}
+
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name AutoAdminLogon -Value 1
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultUserName -Value "vagrant"
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name DefaultPassword -Value "vagrant"
