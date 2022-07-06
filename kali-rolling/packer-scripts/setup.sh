3147ce782d0711bc432ab4d4f3649a7ef8bc33bf
Stian Kristoffersen <stian.kristoffersen@me.com>
2022-07-06 19:34:06 +0200

    Golang

--- kali-rolling/packer-scripts/setup.sh
+++ kali-rolling/packer-scripts/setup.sh
@@ -2,7 +2,7 @@
 
 # Add packages
 apt-get update
-apt-get -y install kali-linux-default python3-pip htop nload gedit dconf-cli bloodhound powershell gss-ntlmssp gobuster
+apt-get -y install kali-linux-default python3-pip htop nload gedit dconf-cli bloodhound powershell gss-ntlmssp gobuster golang
 pip3 install impacket
 gem install evil-winrm 
 
